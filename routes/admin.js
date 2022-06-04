5e0168576d1922c1eb9e2bda33f6b6409c48b20c
musa yıldırım <48180768+musayldrmm@users.noreply.github.com>
2022-06-04 15:37:11 +0300

    admin api yapıldı

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,22 @@
 const express=require('express')
 const router=express.Router()
-const Post = require('../models/program')
-
-router.get('/:id', (req, res) => {
-    userid=req.params.id
-    Post.find({},function(err,docs){
-        res.status(200).send(docs)
-    })
+const User = require("../models/user");
+const Program = require("../models/program")
+router.get('/', (req, res) => {
+    console.log("buradasın kardeşim")
+    const userfind=User.find({})
+    const programfind=Program.find({})
+    Promise.all([
+        userfind,
+        programfind,
+      ]).then(([
+        user,
+        program
+      ]) => {
+       let data ={user,program}
+       console.log(programfind)
+       res.send(200,data)
+      })
 });
 
 module.exports=router
